017234586584cfe65928c316654a5e8736eea89f
Alejandro <josealejandrogomezparedes@gmail.com>
2023-10-05 16:53:51 -0500

    a

--- clases.py
+++ clases.py
@@ -25,6 +25,20 @@ class Notas:
         self.EX2 = ex2
         self.final = Final
 
+    def mostrar_notas(self):
+        list_notas = {
+            "N1": self.N1,
+            "N2": self.N2,
+            "EX1": self.EX1,
+            "P1": self.P1,
+            "N3": self.N3,
+            "N4": self.N4,
+            "P2": self.P2,
+            "EX2": self.EX2,
+            "Final": self.final
+        }
+        return list_notas
+
 
 class asignatura:
 
@@ -36,11 +50,12 @@ class asignatura:
         self.docente = docente_name
         self.matricula = matricula
         self.seccion = seccion
-#        self.estado = "aprobado" if self.notas.final > 70 else "reprobado"
 
     def agregar_notas(self, n1: float, n2: float, p1: float, ex1: float, n3: float, n4: float, p2: float, ex2: float, final: float) -> None:
         self.notas = Notas(n1, n2, p1, ex1,
                            n3, n4, p2, ex2, final)
+
+        self.estado = "aprobado" if self.notas.final > 70 else "reprobado"
 
 
 class usuario:
@@ -53,27 +68,17 @@ class estudiante(usuario):
     def __init__(self, nombre: str, apellido: str, carrera: carrera) -> None:
         super().__init__(nombre, apellido)
         self.carrera = carrera
-        self.__materias = []
+        self.materias = []
 
     def agregar_Materia(self, materia: asignatura) -> None:
-        self.__materias.append(materia)
+        self.materias.append(materia)
 
-    @property
-    def materias(self):
-        list_materias = []
-        for materia in self.__materias:
-            list_materias.append(
-                {
-                    "nombre": materia.nombre,
-                    "docente": materia.docente,
-                    "asistencia": materia.asistencia,
-                    "notas": materia.notas,
-                    "matricula": materia.matricula,
-                    "seccion": materia.seccion,
-                    "nivel": materia.nivel
-                }
-            )
-        return list_materias
+    def obtener_Materia(self, nombre: str) -> asignatura:
+        hallado = False
+        for materia in self.materias:
+            if materia.nombre == nombre:
+                hallado = materia
+        return hallado
 
 
 class docente(usuario):
